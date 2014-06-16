4424fa47109fad4f7de4ce41d34e5daf9f066f97
Asad Palekar <asad.palekar@live.com>
2014-06-16 10:38:03 +0400

    rendering structured JSON

--- SakhrBlog/FlightsTemplate/Trip/Resource/Template/structuredJsonRender.js
+++ SakhrBlog/FlightsTemplate/Trip/Resource/Template/structuredJsonRender.js
@@ -3,16 +3,37 @@ $(document).ready(function () {
     //start rendering an arrayed json. 
     var elem = $('#resultPanel .flightPanel');
     var flights = aJson.flights;
-    var j = 0;
-    while (j < window.RowMultiple) {
-        for (var i = 0; i < flights.length; i++) { // 4 rows. 
-            elem.append(flights[i]);
-        }
-        j++;
-    }
+    $.getJSON('flights.json', function(data) {
+        data.Flights.forEach(function(flight,idx) {
+            var rowText = renderRow(flight);
+            elem.append(rowText);
+        });
+    });
     
+
 });
 
+
+var renderRow = function (flight) {
+
+    var rowText = '<li class="clickable"> \
+                        <div> \
+                            <p class="flight"><img src="' +flight.Image.Source + '" alt="'+ flight.Image.Alt + '" /><span>'+ flight.Name + '</span></p> \
+                            <p class="depart">'+ flight.Departure.Time + '<span class="airlines">British Air..</span></p> \
+                            <ul class="route stop0"> \
+                                <li class="first alternateAirport">'+flight.Departure.Destination +'</li> \
+                                <li class="last">' + flight.Arrival.Destination +'<em>2</em></li> \
+                            </ul> \
+                            <p class="arrive">'+flight.Arrival.Time +'<span class="duration">45h 30m</span></p> \
+                            <p class="duration">' +flight.Duration + '</p> \
+                            <a class="more">more</a> \
+                            <p class="price"><em>'+ flight.Price.Currency + '</em>' + flight.Price.Amount +'</p> \
+                            <p class="sell"><a class=" button blue">Book</a></p> \
+                        </div>\
+                    </li>';
+
+    return rowText;
+}
 
 var aJson = {
     'flights': [
